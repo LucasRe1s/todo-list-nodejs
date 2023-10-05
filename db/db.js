656ddc94f17dc7002ef994a462f112604880cf4f
Lucas Reis <108764828+LucasRe1s@users.noreply.github.com>
2023-10-05 20:45:23 -0300

    feat: update index.js e db.js

--- db/db.js
+++ db/db.js
@@ -1,14 +1,1 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('todo_list', 'root', '12345678', {
-    host: 'localhost',
-    dialect: 'mysql' 
-})
-try {
-    sequelize.authenticate();
-    console.log('Conectado com sucesso.')
-} catch (error) {
-    console.log('Não foi possivel conectar:', error);
-}
-
-module.exports = sequelize;
+const data = [];
--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
-const db = require('./db/db');
+const { UUIDV1 } = require('sequelize');
+// const db = require('./db/db');
 require('dotenv').config()
 
 const app = express();
 
 app.use(express.json())
+const data = [];
 
-app.listen(3000, console.log(`node rodando na porta }`));
+app.get("/task", (req, res) => {
+    console.log(data)
+    // res.json({msg: "retornando db." + data})
+    res.json({data})
+})
+
+
+app.post("/task", (req, res) => {
+
+    const id = UUIDV1;
+    const task = req.body;
+
+    data.push(task + id);
+    res.json({msg: "tarefa adicionada"})
+    console.log(`${task}, ${id}`)
+})
+const PORT = 3000;
+app.listen(PORT || 3000, ()=> {
+    console.log(`node rodando na porta ${PORT}`)
+});
